eff645a591053cfb7835ec6b66d0e066ebf046d0
Giuseppe Dicosola <giuseppe.dicosola.github@outlook.com>
2014-10-02 13:29:46 +0200

    Update 3.23.c

--- exercises/C-How-to-program-6th-edition---Deitel/Chapter-3/solutions/3.23.c
+++ exercises/C-How-to-program-6th-edition---Deitel/Chapter-3/solutions/3.23.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 int main()
 {
-  int i;
-  
-  for (i=1;i<11;i+=1)
-    printf("%4d",i);
+    int i;
     
-  return 0;
+    for (i=1;i<11;i+=1)
+    {
+        printf("%4d",i);
+    }
+    
+    return 0;
 } 
